cb1b4b2de18d32be58e6275e71c4e0c076f69aa6
Crafter-Y <mrcrafter.yt1@gmail.com>
2024-11-01 23:42:24 +0100

    maybe now with version

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import { success } from "../responses";
 import { validateToken } from "../validate";
-import packageJson from "../../package.json"
+import { readFileSync } from "node:fs"
+import path from "node:path";
 
 const router = Router();
 
 router.get("/healthcheck", (req, res) => {
+    const packageJsonFile = readFileSync(path.join(__dirname, "..", "..", "package.json"), { encoding: "utf-8" })
+
     const healthcheck = {
         uptime: process.uptime(),
         message: 'OK',
-        version: packageJson.version,
+        version: JSON.parse(packageJsonFile).version,
         timestamp: Date.now()
     };
     try {
